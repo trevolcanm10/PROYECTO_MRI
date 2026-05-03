3f694196fb544f8ff38ca09e306707be39997ee8
trevolcanm10 <denilson.morales@unmsm.edu.pe>
2026-05-02 19:20:44 -0500

    Eliminacion de la dependencia

--- backend/backend.py
+++ backend/backend.py
@@ -2,7 +2,6 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import tflite_runtime.interpreter as tflite
 import numpy as np
-from tensorflow.keras.preprocessing import image
 import os
 from werkzeug.utils import secure_filename
 import matplotlib
