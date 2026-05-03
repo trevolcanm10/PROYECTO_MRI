ff4b31a4d9cfafc0194e87b0c87d6b11669b955d
trevolcanm10 <denilson.morales@unmsm.edu.pe>
2026-05-02 19:47:06 -0500

    Modificaciones codigo

--- backend/backend.py
+++ backend/backend.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import tflite_runtime.interpreter as tflite
+from tensorflow.keras.preprocessing import image
 import numpy as np
 import os
 from werkzeug.utils import secure_filename
@@ -96,4 +97,4 @@ def clasificar_api():
 # ============================
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", port=port, debug=False)
